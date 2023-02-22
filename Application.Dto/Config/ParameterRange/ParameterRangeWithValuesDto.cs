ed8f69b99a0f79bb4a4a2bdf6edf3693a5eabca1
Royss <martel.royss21@gmail.com>
2023-02-21 23:07:51 -0500

    commit

--- Application.Dto/Config/ParameterRange/ParameterRangeWithValuesDto.cs
+++ Application.Dto/Config/ParameterRange/ParameterRangeWithValuesDto.cs
@@ -4,6 +4,7 @@ namespace Application.Dto.Config.ParameterRange
     using Application.Dto.Config.ParameterValue;
     public class ParameterRangeWithValuesDto : BaseParameterRangeDto
     {
+        public bool IsInternalConfiguration { get; set; }
         public Guid Id { get; set; }
         public List<ParameterValueDto> ParametersValue { get; set; }
     }
